d60aa781b73866edabf98cd38356a9ea58e47335
Faretos <vanyafilipiev@gmail.com>
2025-04-19 16:01:30 +0300

    Блок advantages для планшета, +коррекция кнопок swiperа везде, начало desktop версии

--- source/js/swiper-advantages.js
+++ source/js/swiper-advantages.js
@@ -3,14 +3,14 @@ import {Navigation} from 'swiper/modules';
 
 const advantagesList = document.querySelector('.advantages__list');
 const advantagesListWrapper = document.querySelector('.advantages__list-wrapper');
-const advantagesCard = document.querySelectorAll('.advantages__card');
+const advantagesCards = document.querySelectorAll('.advantages__card');
 let advantagesSwiper = null;
 
 function addSwiper () {
   if (window.innerWidth >= 1440) {
     advantagesList.classList.add('swiper-wrapper');
     advantagesListWrapper.classList.add('swiper');
-    advantagesCard.forEach((card) => card.classList.add('swiper-slide'));
+    advantagesCards.forEach((card) => card.classList.add('swiper-slide'));
 
     if (!advantagesSwiper) {
       advantagesSwiper = new Swiper ('.advantages__list-wrapper', {
@@ -19,14 +19,16 @@ function addSwiper () {
         direction: 'horizontal',
         autoHeight: true,
         loop: true,
-        slidesPerView: 3,
+        initialSlide: Math.floor(advantagesCards.length / 2),
+        slidesPerView: 'auto',
+        centeredSlides: true,
         slidesPerGroup: 2,
         spaceBetween: 30,
         navigation: {
           nextEl: '.advantages__next-slide',
           prevEl: '.advantages__prev-slide',
         },
-        // allowTouchMove: window.innerWidth < 1440,
+        allowTouchMove: window.innerWidth < 1440,
       });
     }
   } else {
@@ -37,7 +39,7 @@ function addSwiper () {
       }
       advantagesList.classList.remove('swiper-wrapper');
       advantagesListWrapper.classList.remove('swiper');
-      advantagesCard.forEach((card) => card.classList.remove('swiper-slide'));
+      advantagesCards.forEach((card) => card.classList.remove('swiper-slide'));
     }
   }
 }
