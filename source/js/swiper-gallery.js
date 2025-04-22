1d51920be9cac941ee8ef6e1e4d187a4082cde85
Faretos <vanyafilipiev@gmail.com>
2025-04-22 20:58:50 +0300

    блок gallery для десктопа, плюс коррекция swiper

--- source/js/swiper-gallery.js
+++ source/js/swiper-gallery.js
@@ -1,28 +1,46 @@
 import Swiper from 'swiper';
 import {Navigation} from 'swiper/modules';
 
-const gallerySwiper = new Swiper ('.gallery__slider', {
-  modules: [Navigation],
+let gallerySwiper = null;
 
-  direction: 'horizontal',
-  loop: true,
-  autoHeight: true,
-  slidesPerView: 2,
-  spaceBetween: 5,
+function initSwiper() {
+  if (window.innerWidth < 1440 && !gallerySwiper) {
+    gallerySwiper = new Swiper('.gallery__slider', {
+      modules: [Navigation],
+      direction: 'horizontal',
+      loop: true,
+      autoHeight: true,
+      slidesPerView: 2,
+      spaceBetween: 5,
+      navigation: {
+        nextEl: '.gallery__next-slide',
+        prevEl: '.gallery__prev-slide',
+      },
+      allowTouchMove: true,
+      breakpoints: {
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+        },
+      },
+    });
+  }
+}
 
-  navigation: {
-    nextEl: '.gallery__next-slide',
-    prevEl: '.gallery__prev-slide',
-  },
-  allowTouchMove: window.innerWidth < 1440,
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 5,
-    },
-    1440: {
-      slidesPerView: 5,
-      spaceBetween: 5,
-    },
-  },
-});
+function destroySwiper() {
+  if (gallerySwiper) {
+    gallerySwiper.destroy(true, true);
+    gallerySwiper = null;
+  }
+}
+
+function handleResize() {
+  if (window.innerWidth < 1440) {
+    destroySwiper();
+  } else {
+    initSwiper();
+  }
+}
+
+window.addEventListener('load', initSwiper);
+window.addEventListener('resize', handleResize);
