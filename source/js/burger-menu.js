a00ff573d364f760203a7b5ccc8a71e2cdbc521b
Faretos <vanyafilipiev@gmail.com>
2025-04-30 19:43:45 +0300

    Завершение макета стр, тесты PP

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,9 +1,10 @@
 const burgerMenuButton = document.querySelector('.header__burger-menu-button');
 const navList = document.querySelector('.header__navigation');
+const navLinks = document.querySelectorAll('.header__navigation-link');
 const headerWrapper = document.querySelector('.header__wrapper');
 const telephone = document.querySelector('.header__telephone');
 
-function burgerMenuToggle () {
+function burgerMenuToggle() {
   if (window.innerWidth >= 1440) {
     return;
   }
@@ -14,14 +15,25 @@ function burgerMenuToggle () {
   telephone.classList.toggle('header__telephone--openned');
 }
 
+function closeBurgerMenu() {
+  burgerMenuButton.classList.remove('header__burger-menu-button--openned');
+  navList.classList.remove('header__navigation--openned');
+  headerWrapper.classList.remove('header__wrapper--nav-openned');
+  telephone.classList.remove('header__telephone--openned');
+}
+
 function updateBurgerMenu() {
   if (window.innerWidth < 1440) {
     burgerMenuButton.addEventListener('click', burgerMenuToggle);
+    navLinks.forEach((link) => {
+      link.addEventListener('click', closeBurgerMenu);
+    });
   } else {
     burgerMenuButton.removeEventListener('click', burgerMenuToggle);
-    navList.classList.remove('header__navigation--openned');
-    headerWrapper.classList.remove('header__wrapper--nav-openned');
-    telephone.classList.remove('header__telephone--openned');
+    navLinks.forEach((link) => {
+      link.removeEventListener('click', closeBurgerMenu);
+    });
+    closeBurgerMenu();
   }
 }
 
